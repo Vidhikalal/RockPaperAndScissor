0e4a9a5e75f2ceb79d9ece3395bab816c035208b
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-28 19:27:02 -0500

    Initialised project ,imported random library ,and created a list

--- RockPaperAndScissor/RockPaperAndScissor.py
+++ RockPaperAndScissor/RockPaperAndScissor.py
@@ -1,1 +1,11 @@
 
+import random
+user_wins=0
+computer_wins=0
+while True:
+    user_input= input("Type Roclk/Paper/Scissors or Q to Quit").lower()
+    if user_input=="q":
+        quit()
+        
+        #creating list where we have rock ,paper and scissor
+    if user_input in ["rock","paper","scissors"]:
