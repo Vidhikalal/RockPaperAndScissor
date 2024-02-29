34749c14099af55c69c2e8e4301087cd6d6ff523
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-28 19:57:03 -0500

    Created game Rock,paper and Scissors

--- RockPaperAndScissor/RockPaperAndScissor.py
+++ RockPaperAndScissor/RockPaperAndScissor.py
@@ -2,10 +2,36 @@
 import random
 user_wins=0
 computer_wins=0
+options=["rock","paper","scissors"]
 while True:
-    user_input= input("Type Roclk/Paper/Scissors or Q to Quit").lower()
+    user_input= input("Type Roclk/Paper/Scissors or Q to Quit:").lower()
     if user_input=="q":
-        quit()
+        break
         
         #creating list where we have rock ,paper and scissor
-    if user_input in ["rock","paper","scissors"]:
+    if user_input not in options:
+        continue 
+    random_number=random.randint(0,2)
+    #rock is 0, paperis 1and scissors is 2
+    computer_pick=options[random_number]
+    print("Computer picked",computer_pick +".")
+    if user_input=="rock" and computer_pick=="scissors":
+        print("You won")
+        user_wins+=1
+        
+    elif user_input=="paper" and computer_pick=="rock":
+        print("You won")
+        user_wins+=1
+        
+    elif user_input=="scissors" and computer_pick=="paper":
+        print("You won")
+        user_wins+=1
+    else:
+        print("You lost")
+        computer_wins+=1
+        
+    print("You won",user_wins,"times.") 
+    print("Computer won",computer_wins,"times.")
+         
+    print("GoodByee!")
+    
